f67e72ac36a66e6a9139dc0ddb35432c644bf243
Andrew Johnston <andrew.johnston@shopify.com>
2019-01-23 14:13:11 -0500

    unit tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,5 +7,6 @@ class Product < ApplicationRecord
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, length: { minimum: 10, message: 'is no can do bucko!' }
 
 end
