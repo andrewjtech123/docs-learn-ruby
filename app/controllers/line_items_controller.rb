940028b7bfbda33c0033ada618c4f6ba4d837dd0
Andrew Johnston <andrew.johnston@shopify.com>
2019-06-27 12:16:50 -0400

    ajax iteration 1

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -39,8 +39,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart,
-          notice: 'Line item was successfully created.' }
+        format.html { redirect_to store_index_url }
         format.json { render :show,
           status: :created, location: @line_item }
       else
@@ -81,7 +80,8 @@ class LineItemsController < ApplicationController
       @line_item = LineItem.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Never trust parameters from the scary internet, only allow the white
+    # list through.
     def line_item_params
       params.require(:line_item).permit(:product_id)
     end
